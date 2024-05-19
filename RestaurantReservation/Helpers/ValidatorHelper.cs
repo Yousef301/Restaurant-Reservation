ea8acc489f0e780795d9f4c80a1b95a0282687c9
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-05-19 13:56:38 +0300

    Create ValidatorHelper class that validates object properties constraints.

---
+++ RestaurantReservation/Helpers/ValidatorHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.Helpers;
+
+public static class ValidatorHelper
+{
+    public static bool TryValidate(object obj, out ICollection<ValidationResult> results)
+    {
+        var context = new ValidationContext(obj, serviceProvider: null, items: null);
+        results = new List<ValidationResult>();
+        return Validator.TryValidateObject(obj, context, results, validateAllProperties: true);
+    }
+}
